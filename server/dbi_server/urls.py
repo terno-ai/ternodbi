cd9c2401cf3a29ed06c15b65fa8ac51dc1da84b6
navinbhagat322 <navinbhagat322@gmail.com>
2026-01-08 17:24:26 +0530

    Added landing page template and update URL configuration

--- server/dbi_server/urls.py
+++ server/dbi_server/urls.py
@@ -3,9 +3,11 @@ URL configuration for DBI Server.
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from dbi_layer.django_app.views import landing_page
 
 urlpatterns = [
+    path('', landing_page, name='home'),
     path('admin/', admin.site.urls),
+    path('dbi/', include('dbi_layer.django_app.urls')),
 ]
-
